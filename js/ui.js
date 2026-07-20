ceb17d625f67e411574d4298b91b24888aa2cd58
osmaniyeproje-coder <osmaniyeproje@gmail.com>
2026-07-20 15:21:49 +0300

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -1,1 +1,34 @@
-// boş
+const UI = {
+
+    init() {
+
+        this.renderLeaderboard();
+
+    },
+
+    renderLeaderboard() {
+
+        const scoreTable = document.getElementById("scoreTable");
+
+        if (!scoreTable) return;
+
+        const players = Players.getAll();
+
+        let html = "";
+
+        players.forEach(player => {
+
+            html += `
+                <div class="scoreItem">
+                    <strong>${player.name}</strong>
+                    <span style="float:right;">0 Puan</span>
+                </div>
+            `;
+
+        });
+
+        scoreTable.innerHTML = html;
+
+    }
+
+};
