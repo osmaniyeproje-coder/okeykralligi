0a7a831c78af033124aab1a63b64030e1b1067d0
osmaniyeproje-coder <osmaniyeproje@gmail.com>
2026-07-20 15:22:48 +0300

    Update history.js

--- js/history.js
+++ js/history.js
@@ -1,2 +1,41 @@
+const History = {
 
-// boş
+    render() {
+
+        const area = document.getElementById("historyList");
+
+        if (!area) return;
+
+        const games = Games.getAll();
+
+        if (games.length === 0) {
+
+            area.innerHTML = "Henüz oyun oynanmadı.";
+
+            return;
+
+        }
+
+        let html = "";
+
+        games.reverse().forEach(game => {
+
+            html += `
+                <div class="historyItem">
+
+                    <strong>${game.date}</strong>
+
+                    <br>
+
+                    ${game.type}
+
+                </div>
+            `;
+
+        });
+
+        area.innerHTML = html;
+
+    }
+
+};
