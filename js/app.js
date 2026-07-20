9eee74115e9fc7598655aee3768c006f298d37c5
osmaniyeproje-coder <osmaniyeproje@gmail.com>
2026-07-20 15:21:21 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
-// app.js
+const App = {
+
+    init() {
+
+        console.log("👑 Okey Krallığı Başlatıldı");
+
+        Storage.init();
+
+        Players.load();
+
+        UI.init();
+
+        Games.load();
+
+    }
+
+};
