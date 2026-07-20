e7e9311075aaf13c30d3d8d887eea109bd36c969
osmaniyeproje-coder <osmaniyeproje@gmail.com>
2026-07-20 15:19:57 +0300

    Update storage.js

--- js/storage.js
+++ js/storage.js
@@ -1,1 +1,43 @@
-// boş
+const Storage = {
+
+    KEY: "okeyDB",
+
+    init() {
+
+        if (!localStorage.getItem(this.KEY)) {
+
+            const database = {
+
+                season: "Temmuz 2026",
+
+                players: [],
+
+                games: [],
+
+                settings: {
+                    adminPassword: "baho123"
+                },
+
+                rules: []
+
+            };
+
+            localStorage.setItem(this.KEY, JSON.stringify(database));
+
+        }
+
+    },
+
+    get() {
+
+        return JSON.parse(localStorage.getItem(this.KEY));
+
+    },
+
+    save(data) {
+
+        localStorage.setItem(this.KEY, JSON.stringify(data));
+
+    }
+
+};
