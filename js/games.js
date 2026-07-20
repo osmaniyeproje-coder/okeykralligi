bf777fb164df81d8d88652d8e940239396fee48f
osmaniyeproje-coder <osmaniyeproje@gmail.com>
2026-07-20 15:22:29 +0300

    Update games.js

--- js/games.js
+++ js/games.js
@@ -1,2 +1,41 @@
+const Games = {
 
-// boş
+    load() {
+
+        const db = Storage.get();
+
+        if (!db.games) {
+
+            db.games = [];
+
+            Storage.save(db);
+
+        }
+
+    },
+
+    add(game) {
+
+        const db = Storage.get();
+
+        game.id = Date.now();
+
+        game.date = new Date().toLocaleString("tr-TR");
+
+        db.games.push(game);
+
+        Storage.save(db);
+
+        UI.renderLeaderboard();
+
+        UI.renderHistory();
+
+    },
+
+    getAll() {
+
+        return Storage.get().games;
+
+    }
+
+};
